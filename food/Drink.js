58388bba1cc7d4e1ecb6994e8376cd30768b4515
dgarascenco <54360508+dgarascenco@users.noreply.github.com>
2021-02-12 13:14:02 +0200

    Add files via upload

--- food/Drink.js
+++ food/Drink.js
@@ -4,4 +4,13 @@ module.exports = class Drink extends Item {
         super(id, name, price, volume);
         this.volume = volume
     } 
+    print(){
+        let text = `${this.name} (${this.volume.value} ${this.volume.unit})`
+        let id = this.id.toString().padEnd(2);
+        let name = text.padStart(15 + text.length/2).padEnd(30);
+        let value = this.price.value.toString().padStart(3);
+        let currency = this.price.currency.toString().padStart(4);
+        return  `${id}: ${name} : ${value} ${currency}\n` +
+                `----------------------------------------------`;
+    }
 }
--- food/Food.js
+++ food/Food.js
@@ -4,4 +4,14 @@ module.exports = class Food extends Item {
         super(id, name, price, weight);
         this.weight = weight
     } 
+
+    print(){
+        let text = `${this.name} (${this.weight.value} ${this.weight.unit})`
+        let id = this.id.toString().padEnd(2);
+        let name = text.padStart(15 + text.length/2).padEnd(30);
+        let value = this.price.value.toString().padStart(3);
+        let currency = this.price.currency.toString().padStart(4);
+        return  `${id}: ${name} : ${value} ${currency}\n` +
+                `----------------------------------------------`;
+    }
 }
