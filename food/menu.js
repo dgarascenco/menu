6eceedd97bf642804fa0dee7eb0ab767c4f7143c
dgarascenco <54360508+dgarascenco@users.noreply.github.com>
2021-02-12 13:15:10 +0200

    Add files via upload

--- food/menu.js
+++ food/menu.js
@@ -6,5 +6,5 @@ const Measurement = require("./physical")
 module.exports.menu = [
     new Food(1, "Salad", new Price(15.00, "MDL"), new  Measurement(0.3,"Kg")),
     new Drink(25, "Soup", new Price(25.00, "MDL"), new Measurement(0.5,"L")),
-    new Food(3, "Bread оро рор",new Price(5.00, "MDL"), new  Measurement(50,"g"))
+    new Food(3, "Bread with meat",new Price(5.00, "MDL"), new  Measurement(50,"g"))
 ]
